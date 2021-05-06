a49c503f554b96f79539fb090e6c467cfdefde2d
RadugaDuga <okretans@mail.ru>
2021-05-07 00:42:21 +0400

    UsersAPI

---
+++ projects/project-01/easy-react-01/src/API/api.js
@@ -0,0 +1,25 @@
+import  axios  from "axios";
+
+const mainUrl = ""
+
+const instanse = axios.create({
+    baseURL:"https://social-network.samuraijs.com/api/1.0/",
+    withCredentials:true,
+    headers:{
+        "API-KEY": "7fc96318-cc16-423f-b993-464136a7cea1"
+    }
+})
+
+
+// usersAPI — обьект с методами для работы с юзерами
+export const usersAPI = {
+
+    getUsersData(currentPageNum = 1, pageSize = 8) {
+        return instanse.get(`users?page=${currentPageNum}&count=${pageSize}`,
+        {
+            withCredentials:true
+        })
+        .then( response => response.data )
+    }
+
+}
--- projects/project-01/easy-react-01/src/components/Users/UsersAPI.jsx
+++ projects/project-01/easy-react-01/src/components/Users/UsersAPI.jsx
@@ -3,31 +3,32 @@ import React from "react";
 import Users from "./Users";
 import "axios-progress-bar/dist/nprogress.css";
 import { loadProgressBar } from "axios-progress-bar";
-
+import { usersAPI } from './../../API/api';
 
 loadProgressBar();
 
+
 class UsersAPI extends React.Component {
+
 	componentDidMount = () => {		
-	axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPageNum}&count=${this.props.pageSize}`,{withCredentials:true})
-		 .then( response => {
-			this.props.setUsers(response.data.items);
-			this.props.setUsersTotalCount(response.data.totalCount);
-		 });
+		usersAPI.getUsersData(this.props.currentPageNum, this.props.pageSize).then( data => {
+			this.props.setUsers(data.items);
+			this.props.setUsersTotalCount(data.totalCount);
+		});
 		
 	}
 
 	onPageChanged = ( currentPageNum ) => {
 		this.props.setCurrentPageNum(currentPageNum);
-		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPageNum}&count=${this.props.pageSize}`, {withCredentials:true})
-			 .then( response => {
-				this.props.setUsers(response.data.items);
-			 });
+		usersAPI.getUsersData(currentPageNum, this.props.pageSize).then( data => {
+			this.props.setUsers(data.items);
+		});
 	};
 
 	render() {
 		return <Users {...this.props} onPageChanged ={this.onPageChanged}/>
 	}
+
 }
 
 export default UsersAPI;
