42f74f2b1dfb19208be84f1bee147bed8304937c
RadugaDuga <okretans@mail.ru>
2021-05-23 18:00:00 +0400

    End 80%

--- easy-react-01/src/App.js
+++ easy-react-01/src/App.js
@@ -3,13 +3,16 @@ import s from "./App.module.css";
 import HeaderContainer from "./components/Header/HeaderContainer.js";
 import Nav from "./components/Navigation/Nav";
 import { Route } from "react-router-dom";
-import Dialogs_Container from "./components/Dialogs/Dialogs_Container";
 import Groups_Container from "./components/Groups/Groups_Container";
 import Users_Container from "./components/Users/Users_Container";
 import Profile_Container from "./components/Profile/ProfileContainer";
 import Login from "./components/Login/Login";
 import { initializeApp } from "./redux/app-reducer";
 import { connect } from "react-redux";
+import { withSuspence } from './components/HOC/withSuspense';
+const Dialogs_Container = React.lazy(() =>import("./components/Dialogs/Dialogs_Container"))
+
+
 
 class App extends React.Component {
 	componentDidMount() {
@@ -28,11 +31,11 @@ class App extends React.Component {
 					<HeaderContainer />
 					<Nav />
 					<div className={s.app_wrapper_content}>
+						<Route path="/profile/:userId?" render={() => <Profile_Container />}/>
 						<Route
-							path="/profile/:userId?"
-							render={() => <Profile_Container />}
+							path="/dialogs"
+							render={ withSuspence(Dialogs_Container) }
 						/>
-						<Route path="/dialogs" render={() => <Dialogs_Container />} />
 						<Route path="/users" render={() => <Users_Container />} />
 						<Route path="/groups" render={() => <Groups_Container />} />
 						<Route path="/login" render={() => <Login />} />
@@ -42,6 +45,8 @@ class App extends React.Component {
 		);
 	}
 }
+
+
 const mapStateToProps = (state) => ({
 	initialized: state.app.initialized,
 });
