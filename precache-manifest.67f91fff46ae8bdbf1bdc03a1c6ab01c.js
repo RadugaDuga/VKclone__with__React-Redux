cd8dcc081fa800bc2636a834e6f02c1ac58ec265
RadugaDuga <okretans@mail.ru>
2021-05-23 20:17:55 +0400

    Updates

---
+++ precache-manifest.67f91fff46ae8bdbf1bdc03a1c6ab01c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642b71de3befcb6248e080d690c7c5ac",
+    "url": "/VKladovke/index.html"
+  },
+  {
+    "revision": "6ab18ad77b3afe5075f1",
+    "url": "/VKladovke/static/css/2.ff33e90f.chunk.css"
+  },
+  {
+    "revision": "221c53aa1680de73899c",
+    "url": "/VKladovke/static/css/3.f70fb955.chunk.css"
+  },
+  {
+    "revision": "34baabb1d71e408ed3b9",
+    "url": "/VKladovke/static/css/main.c657e70a.chunk.css"
+  },
+  {
+    "revision": "6ab18ad77b3afe5075f1",
+    "url": "/VKladovke/static/js/2.3dc0fcdc.chunk.js"
+  },
+  {
+    "revision": "b18f4a1569f46bdd20a005ce3bc53a10",
+    "url": "/VKladovke/static/js/2.3dc0fcdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221c53aa1680de73899c",
+    "url": "/VKladovke/static/js/3.3e0bdce3.chunk.js"
+  },
+  {
+    "revision": "34baabb1d71e408ed3b9",
+    "url": "/VKladovke/static/js/main.7045673d.chunk.js"
+  },
+  {
+    "revision": "11d01f4196cb6d4ef97b",
+    "url": "/VKladovke/static/js/runtime-main.ee2df0a5.js"
+  },
+  {
+    "revision": "7f18494cddfae6665ca61666cac7cf36",
+    "url": "/VKladovke/static/media/add-message-button.7f18494c.svg"
+  },
+  {
+    "revision": "ebc0d8a8bcc14ada8e34e2955073eb1a",
+    "url": "/VKladovke/static/media/backgroundVk.ebc0d8a8.png"
+  },
+  {
+    "revision": "4ea0b44ddcf2e5079f0febea7e5a6bea",
+    "url": "/VKladovke/static/media/bell.4ea0b44d.svg"
+  },
+  {
+    "revision": "4532a17e7770ac99d3462278282e556b",
+    "url": "/VKladovke/static/media/clyps.4532a17e.svg"
+  },
+  {
+    "revision": "7bbbfbc5a225079936ae92d3dab816d5",
+    "url": "/VKladovke/static/media/comment.7bbbfbc5.svg"
+  },
+  {
+    "revision": "e728d53c970858ff896621118dc7ede0",
+    "url": "/VKladovke/static/media/cyberpunk.e728d53c.jpg"
+  },
+  {
+    "revision": "f5941bf9ef9238decdddf7e3fbe3cf2e",
+    "url": "/VKladovke/static/media/friends.f5941bf9.svg"
+  },
+  {
+    "revision": "b0d780968abe993abd1862605530db85",
+    "url": "/VKladovke/static/media/groups.b0d78096.svg"
+  },
+  {
+    "revision": "ac1b967c49cd656d4e667d5333774866",
+    "url": "/VKladovke/static/media/iconClock.ac1b967c.svg"
+  },
+  {
+    "revision": "df7488fbf4aff2fbbd2de8cb50e0851f",
+    "url": "/VKladovke/static/media/iconMoney.df7488fb.svg"
+  },
+  {
+    "revision": "4ed24f737450bf96c27a10af410e2d97",
+    "url": "/VKladovke/static/media/likeHovered.4ed24f73.svg"
+  },
+  {
+    "revision": "f25da45492e28168df917dc89634bd14",
+    "url": "/VKladovke/static/media/liked.f25da454.svg"
+  },
+  {
+    "revision": "082b66501f5ed4638677bb72ee50c165",
+    "url": "/VKladovke/static/media/messenger.082b6650.svg"
+  },
+  {
+    "revision": "4af3002293e36aa5a6a6384187986e1e",
+    "url": "/VKladovke/static/media/music.4af30022.svg"
+  },
+  {
+    "revision": "261eabc06d22835bcc478ca53137ac3d",
+    "url": "/VKladovke/static/media/musicBtn.261eabc0.svg"
+  },
+  {
+    "revision": "9f00353cf66a13b6e4841ec95bcdbfbf",
+    "url": "/VKladovke/static/media/news.9f00353c.svg"
+  },
+  {
+    "revision": "15c6d40310f6347539b7f7183c072cb5",
+    "url": "/VKladovke/static/media/photos.15c6d403.svg"
+  },
+  {
+    "revision": "00c2622bad8f24a64db13326dd54f135",
+    "url": "/VKladovke/static/media/plus-button.00c2622b.svg"
+  },
+  {
+    "revision": "71eb9cbd5bb49674120541261789d139",
+    "url": "/VKladovke/static/media/profile.71eb9cbd.svg"
+  },
+  {
+    "revision": "3aa69445003f662959cf52ecb1bd42e0",
+    "url": "/VKladovke/static/media/repost.3aa69445.svg"
+  },
+  {
+    "revision": "0dec89fe21d09719818c47084eb50521",
+    "url": "/VKladovke/static/media/searchBack.0dec89fe.svg"
+  },
+  {
+    "revision": "1618a09fa9f9e800e1788c6d6601b36e",
+    "url": "/VKladovke/static/media/smile-button.1618a09f.svg"
+  },
+  {
+    "revision": "45e737230d7eb70383d88fe1c8b8edf9",
+    "url": "/VKladovke/static/media/unliked.45e73723.svg"
+  },
+  {
+    "revision": "4532a17e7770ac99d3462278282e556b",
+    "url": "/VKladovke/static/media/videos.4532a17e.svg"
+  },
+  {
+    "revision": "c99ed0099f65663731d69c3916be23b4",
+    "url": "/VKladovke/static/media/views.c99ed009.svg"
+  },
+  {
+    "revision": "7a84d436f2bf1cd35ab8e94ae500b623",
+    "url": "/VKladovke/static/media/vk2.7a84d436.svg"
+  }
+]);
