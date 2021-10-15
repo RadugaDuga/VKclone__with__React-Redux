479d813e5e51e4f007461e832739da915a7bf8f8
Georgi Bukia <okretans@mail.ru>
2021-10-15 17:07:56 +0700

    Первый пул

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,63 +1,55 @@
 import React from "react";
 import Profile from "./Profile";
 import { connect } from "react-redux";
-import {getStatus,getUserProfile} from "./../../redux/profile-reducer";
+import { getStatus, getUserProfile } from "./../../redux/profile-reducer";
 import { withRouter } from "react-router-dom";
 import { withAuthRedirect } from "../HOC/withAuthRedirect";
 import { compose } from "redux";
 
-
-
-
-
 const mapStateToProps = (state) => {
-	return {
-		profile: state.profilePage.profile,
-		status: state.profilePage.status,
-		authorizedUserId: state.auth.id,
-		isAuth: state.auth.isAuth,
-	};
+  return {
+    profile: state.profilePage.profile,
+    status: state.profilePage.status,
+    authorizedUserId: state.auth.id,
+    isAuth: state.auth.isAuth,
+  };
 };
 
-
 class ProfileContainer extends React.PureComponent {
-	//Рефреш нужен для получения пользователя и его данных и он вынесен в функцию т.к повторяется
+  //Рефреш нужен для получения пользователя и его данных и он вынесен в функцию т.к повторяется
 
-	refreshProfile() {
-		let userId = this.props.match.params.userId;
-		if (!userId) {
-			userId = this.props.authorizedUserId;
-		}
-		this.props.getUserProfile(userId);
-		this.props.getStatus(userId);
-	}
+  refreshProfile() {
+    let userId = this.props.match.params.userId;
+    if (!userId) {
+      userId = this.props.authorizedUserId;
+    }
+    this.props.getUserProfile(userId);
+    this.props.getStatus(userId);
+  }
 
-	componentDidMount() {
-		this.refreshProfile();
-	}
+  componentDidMount() {
+    this.refreshProfile();
+  }
 
-	componentDidUpdate(prevProps, prevState) {
-		if (this.props.match.params.userId !== prevProps.match.params.userId) {
-			this.refreshProfile();
-		}
-	}
-	
-	render() {
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.match.params.userId !== prevProps.match.params.userId) {
+      this.refreshProfile();
+    }
+  }
 
-		return (
-			<Profile
-				isOwner={!this.props.match.params.userId}
-				profile={this.props.profile}
-				status={this.props.status}
-			/>
-			
-		);
-		
-	}
+  render() {
+    return (
+      <Profile
+        isOwner={!this.props.match.params.userId}
+        profile={this.props.profile}
+        status={this.props.status}
+      />
+    );
+  }
 }
 
 export default compose(
-	connect(mapStateToProps, { getUserProfile, getStatus}),
-	withRouter,
-	withAuthRedirect
+  connect(mapStateToProps, { getUserProfile, getStatus }),
+  withRouter,
+  withAuthRedirect
 )(ProfileContainer);
